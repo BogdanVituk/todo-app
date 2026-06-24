82697a8b3d4efb372bfbf28fd85df648d541a35a
Vityuk <jokernoelr@gmail.com>
2026-06-24 16:55:02 +0300

    fix: remove tsbuildinfo from repository

--- back/src/prisma/prisma.service.ts
+++ back/src/prisma/prisma.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import 'dotenv/config';
+import { env } from 'prisma/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-   const adapter = new PrismaMariaDb(process.env.DATABASE_URL as string);
+   const adapter = new PrismaMariaDb(env("DATABASE_URL"));
     super({ adapter });
   }
 }
