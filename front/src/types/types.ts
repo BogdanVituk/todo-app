34a6cfac13e150baaac814a0ddc8733254b75fe4
Vityuk <jokernoelr@gmail.com>
2026-06-25 12:44:01 +0300

    feat(types): add Priority, FilterPriority, FilterState types

--- front/src/types/types.ts
+++ front/src/types/types.ts
@@ -1,11 +1,21 @@
 export type FilterStatus = 'all' | 'active' | 'completed';
+export type Priority = 'LOW' | 'MEDIUM' | 'HIGH';
+export type FilterPriority = 'all' | 'LOW' | 'MEDIUM' | 'HIGH';
 
 export interface Todo {
     id: number;
     title: string;
     description: string;
+    priority?: Priority;
+    deadline?: string;
     completed: boolean;
 }
 
 
 export type TodoRequestBody = Omit<Todo, "id" | "completed">;
+
+export interface FilterState {
+  status: FilterStatus;
+  query: string;
+  priority: FilterPriority;
+}
